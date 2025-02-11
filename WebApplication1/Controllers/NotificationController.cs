460dae17b1e685c0495578ed9a9b6bc7c9fb9883
shulu <shu.lu@smartdesign.com.tw>
2025-02-11 11:06:15 +0800

    feat: add window notification

---
+++ WebApplication1/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
